137912cfe8b701ef2e155c9e417e928b1eb7effc
Haris Sadik  Desai <harisdesai007@gmail.com>
2026-06-26 00:23:42 +0530

    Time: 1 ms (99.95%), Space: 77.2 MB (65.31%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int localMax = nums[0];
+        
+        for(int i = 1; i < nums.length; i++){
+            localMax =  Math.max(nums[i],localMax+nums[i]);
+            max = Math.max(localMax,max);
+      
+        }
+        return max;
+    }
+}
