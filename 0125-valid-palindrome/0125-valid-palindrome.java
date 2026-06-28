af8a23f041152f430f1e605ff8787e17cbe0d70e
Haris Sadik  Desai <harisdesai007@gmail.com>
2026-06-28 16:25:12 +0530

    Time: 15 ms (20.62%), Space: 46.2 MB (32.1%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String clean = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int i = 0;
+        int j = clean.length()-1;
+
+        while(i < j){
+            if(clean.charAt(i) != clean.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+
+    }
+}
